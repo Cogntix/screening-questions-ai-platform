b5861c89798a67fa75b2f11cca18285c4413be1b
GeerthiShan <geerthiga@cogntix.com>
2025-04-25 10:49:31 +0530

    moved the baseURL and PORT number into .env file

--- backend/index.js
+++ backend/index.js
@@ -16,4 +16,5 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/pdf", pdfRoutes);
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
